83ac426af9b6e376e458fae26936b3c8d064ef76
MykolaBieloivanov <mickbieloivanov@gmail.com>
2024-11-15 13:07:24 +0100

    'Updated'

---
+++ src/test/java/com/ait/qa49/FirstSeleniumTest.java
@@ -0,0 +1,50 @@
+package com.ait.qa49;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FirstSeleniumTest {
+
+    WebDriver driver;
+
+    // before  - setUp
+    @BeforeMethod
+    public void setUp() {
+
+        driver = new ChromeDriver();
+//      driver.get("https://www.google.com/");
+        //maximize browser to window
+
+
+        driver.manage().window().maximize();
+
+
+        driver.navigate().to("https://ilcarro.web.app/search");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.navigate().to("https://www.google.com/");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+    }
+
+    //test
+    @Test
+    public void openGoogleTest() {
+        System.out.println("Google opened!");
+    }
+
+
+    //after - tearDown
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
